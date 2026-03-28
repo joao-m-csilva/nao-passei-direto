e0d21c8370d9503196e2f0ae810024effb398e00
João Marcos de Carvalho e Silva <joaomarcos@Joaos-MacBook-Pro.local>
2026-03-28 20:53:17 -0300

    implemented newdb

--- index.php
+++ index.php
@@ -1,14 +1,25 @@
+<?php 
+require_once "src/includes/acess_db.php";
+
+$email = $_POST['email'];
+$senha = md5($_POST['senha']);
+
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" href="../nao-passei-direto/src/styles/login-style.css">
+    <link rel="stylesheet" href="src/styles/login-style.css">
     <title>Não Passei Direto</title>
 </head>
 <body>
-    <form action="#" autocomplete="off" method="post">
+    <form action="index.php" autocomplete="off" method="post">
         <h2>Sistema de Alunos</h2>
         <div>
             
