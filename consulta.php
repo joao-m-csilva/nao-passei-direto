5eebb45adec2bb30dea9e6aceed27d0b47cea562
tibarcellos1 <tibarcellos1@gmail.com>
2026-03-31 03:38:28 -0300

    Add files via upload

---
+++ consulta.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="./styles/cadastro-aluno.css">
+</head>
+
+<body>
+
+<div class="container">
+    <div class="secao-principal">
+
+        <h1>Consulta de Alunos</h1>
+
+<form method="GET">
+    <input type="text" name="buscar" placeholder="Digite o nome do aluno">
+    <button type="submit">Buscar</button>
+</form>
+
+<?php
+$db = new SQLite3('database/alunos.db');
+
+$busca = $_GET['buscar'] ?? '';
+
+$result = $db->query("SELECT * FROM alunos WHERE nome LIKE '%" . $busca . "%'");
+
+while ($aluno = $result->fetchArray()) {
+    echo "<p>Nome: " . $aluno['nome'] . "<br>Email: " . $aluno['email'] . "</p>";
+}
+?>
+
+    </div>
+</div>
+
+</body>
+</html>
